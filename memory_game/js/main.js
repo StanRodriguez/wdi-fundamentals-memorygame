4b5cc3bcbd55163a39823621a8f15276461ef752
Student_06 <Student_06@PSXMA106.local>
2018-12-02 20:55:55 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,21 +22,45 @@ let cards = [
 ];
 let cardsInPlay = [];
 
-let checkForMatch = ()=>{
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
-	} else {
-		console.log("Sorry, try again.");
+let checkForMatch = (card)=>
+// function checkForMatch(card)
+{
+	 
+	card.setAttribute("src", cards[cardId].cardImage);
+	 // console.log(cardsInPlay);
+	if (cardsInPlay.length ===2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
 	}
-}
-
-let flipCard = (cardId) =>{
-	console.log("User flipped " + cards[cardId].rank);
-	console.log("User flipped " + cards[cardId].suit);
-	console.log("User flipped " + cards[cardId].cardImage);
-	cardsInPlay.push(cards[cardId].rank);
 	
 }
-flipCard(0);
-flipCard(2);
-checkForMatch();
+//why does not work in es6?
+// let flipCard = () =>
+function flipCard () {
+	 cardId = this.getAttribute('data-id');
+	// console.log(this);
+	// console.log("User flipped " + cards[cardId].rank);
+	// console.log("User flipped " + cards[cardId].suit);
+	// console.log("User flipped " + cards[cardId].cardImage);
+	console.log(cards[cardId].rank)
+	cardsInPlay.push(cards[cardId].rank);
+	checkForMatch(this);
+	
+}
+let createBoard = () =>{
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+createBoard();
+document.getElementById("reset").addEventListener("click",function () {
+	location.reload();
+})
+
