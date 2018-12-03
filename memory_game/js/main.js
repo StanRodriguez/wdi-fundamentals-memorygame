8694461a4bdaeadde7727e405476f57f50f7a6bf
Student_06 <Student_06@PSXMA106.local>
2018-12-02 19:24:28 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,1 +1,18 @@
-let cards = ["queen","king","queen","king"];
+let cards = ["queen","queen","king","king"];
+let cardsInPlay = [];
+let card1 = cards[0];
+let card2 = cards[1];
+
+cardsInPlay.push(card1);
+
+cardsInPlay.push(card2);
+
+if (cardsInPlay.length ===2) {
+	if (cardsInPlay[0] === cardsInPlay[1] ) {
+		console.log('you found a match');
+	} else {
+		console.log('Sorry, try again.');
+	}
+}
+
+console.log(cardsInPlay);
