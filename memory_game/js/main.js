b8eaaf82c20b0509fe923eab8542a4dbae48ec8f
Student_06 <Student_06@PSXMA106.local>
2018-12-02 19:45:47 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,18 @@
 let cards = ["queen","queen","king","king"];
 let cardsInPlay = [];
-let card1 = cards[0];
-let card2 = cards[1];
 
-cardsInPlay.push(card1);
-
-cardsInPlay.push(card2);
-
-if (cardsInPlay.length ===2) {
-	if (cardsInPlay[0] === cardsInPlay[1] ) {
-		console.log('you found a match');
+let checkForMatch = ()=>{
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
 	} else {
-		console.log('Sorry, try again.');
+		console.log("Sorry, try again.");
 	}
 }
 
-console.log(cardsInPlay);
+let flipCard = (cardId) =>{
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+flipCard(0);
+flipCard(2);
