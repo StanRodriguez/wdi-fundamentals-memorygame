eac6b3560193e7050aa28586c5945b9ea553e152
Student_06 <Student_06@PSXMA106.local>
2018-12-02 19:55:34 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-let cards = ["queen","queen","king","king"];
+let cards = [
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: 'images/queen-of-hearts.png'
+	},
+	{
+		rank: 'queen',
+		suit: 'diamonds',
+		cardImage: 'images/queen-of-diamonds.png'
+	},
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: 'images/king-of-hearts.png'
+	},
+	{
+		rank: 'king',
+		suit: 'diamonds',
+		cardImage: 'images/king-of-diamonds.png'
+	}
+];
 let cardsInPlay = [];
 
 let checkForMatch = ()=>{
@@ -10,9 +31,12 @@ let checkForMatch = ()=>{
 }
 
 let flipCard = (cardId) =>{
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
-	checkForMatch();
+	console.log("User flipped " + cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].suit);
+	console.log("User flipped " + cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
+	
 }
 flipCard(0);
 flipCard(2);
+checkForMatch();
